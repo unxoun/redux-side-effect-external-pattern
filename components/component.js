f363bf00c1c3632434a7413a7f5335e8caed8256
unxoun <60313778+unxoun@users.noreply.github.com>
2022-05-05 10:12:15 -0800

    Update component.js

--- components/component.js
+++ components/component.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import asyncAPI from "../../tools/async-api";
 
 // actions
-import { messageUpdated } from "../../store/slices/message";
+import { messageUpdated } from "../store/slices/message";
 
 //
 export default function ReduxAsyncExternal() {
@@ -11,13 +11,13 @@ export default function ReduxAsyncExternal() {
   const dispatch = useDispatch();
 
   //
-  const fetchAndSetData = async () => {
+  const fetchAndSetMessage = async () => {
     const result = await asyncAPI.call();
     dispatch(messageUpdated(result.data.message));
   };
 
   useEffect(() => {
-    fetchAndSetData();
+    fetchAndSetMessage();
   }, []);
 
   //
