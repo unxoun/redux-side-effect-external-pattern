d7a6c5eb9398c2e96471873f8a0c5b4dace1a9aa
unxoun <60313778+unxoun@users.noreply.github.com>
2022-05-05 10:13:54 -0800

    Update a-component.js

--- a-component.js
+++ a-component.js
@@ -1,8 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import asyncAPI from "../../tools/async-api";
 
-// actions
+// actions creators:
 import { messageUpdated } from "../store/slices/message";
 
 //
